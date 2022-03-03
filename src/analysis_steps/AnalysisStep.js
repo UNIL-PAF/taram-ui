774e1a53e34b0056287cd6d58e0137f005c29d5d
Roman Mylonas <roman.mylonas@sib.swiss>
2022-03-03 17:55:38 +0100

    adapt step name.

--- src/analysis_steps/AnalysisStep.js
+++ src/analysis_steps/AnalysisStep.js
@@ -7,9 +7,9 @@ export default function AnalysisStep(props) {
             <h3>Analysis #{props.data.idx + 1}</h3>
             <div>
                 {
-                    props.data.analysisSteps.map(step => {
+                    props.data.analysisSteps && props.data.analysisSteps.map(step => {
                         switch (step.type) {
-                            case 'initial_result':
+                            case 'initial-result':
                                 return <InitialResult resultId={props.data.resultId} data={step} key={step.id}/>
                                 break
                         }
