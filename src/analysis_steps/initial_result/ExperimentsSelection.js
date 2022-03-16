ef9202dbf36ad5f71e7b2c16e2e3fdae95cd72ab
Roman Mylonas <roman.mylonas@sib.swiss>
2022-03-16 14:26:57 +0100

    Change experiments names.

--- src/analysis_steps/initial_result/ExperimentsSelection.js
+++ src/analysis_steps/initial_result/ExperimentsSelection.js
@@ -1,5 +1,7 @@
-import React, {useEffect, useState} from "react";
-import { Table, Input, Divider } from 'antd';
+import React, {useEffect, useState, useRef, useContext} from "react";
+import { Table, Input, Form } from 'antd';
+
+const EditableContext = React.createContext(null);
 
 export default function ExperimentsSelection(props) {
 
@@ -16,11 +18,107 @@ export default function ExperimentsSelection(props) {
            return  e}))
     }, [props])
 
+    const EditableCell = ({
+                              title,
+                              editable,
+                              children,
+                              dataIndex,
+                              record,
+                              handleSave,
+                              ...restProps
+                          }) => {
+        const [editing, setEditing] = useState(false);
+        const inputRef = useRef(null);
+        const form = useContext(EditableContext);
+        useEffect(() => {
+            if (editing) {
+                inputRef.current.focus();
+            }
+        }, [editing]);
+
+        const toggleEdit = () => {
+            setEditing(!editing);
+            form.setFieldsValue({
+                [dataIndex]: record[dataIndex],
+            });
+        };
+
+        const save = async () => {
+            try {
+                const values = await form.validateFields();
+                toggleEdit();
+                handleSave({ ...record, ...values });
+            } catch (errInfo) {
+                console.log('Save failed:', errInfo);
+            }
+        };
+
+        let childNode = children;
+
+        if (editable) {
+            childNode = editing ? (
+                <Form.Item
+                    style={{
+                        margin: 0,
+                    }}
+                    name={dataIndex}
+                    rules={[
+                        {
+                            required: true,
+                            message: `${title} is required.`,
+                        },
+                    ]}
+                >
+                    <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+                </Form.Item>
+            ) : (
+                <div
+                    className="editable-cell-value-wrap"
+                    style={{
+                        paddingRight: 24,
+                    }}
+                    onClick={toggleEdit}
+                >
+                    {children}
+                </div>
+            );
+        }
+
+        return <td {...restProps}>{childNode}</td>;
+    };
+
+    const EditableRow = ({ index, ...props }) => {
+        const [form] = Form.useForm();
+        return (
+            <Form form={form} component={false}>
+                <EditableContext.Provider value={form}>
+                    <tr {...props} />
+                </EditableContext.Provider>
+            </Form>
+        );
+    };
+
+    const handleSave = (row) => {
+        console.log(row)
+        const newData = [...this.state.dataSource];
+        const index = newData.findIndex((item) => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        this.setState({
+            dataSource: newData,
+        });
+    };
+
+
     const columns = [
         {
             title: 'Experiment name',
             dataIndex: 'name',
-            render: (text) => <Input type="text" defaultValue={text}/>
+            onCell: (record) => ({
+                record,
+                editable: true,
+                handleSave: handleSave
+            }),
         },
         {
             title: 'File name',
@@ -28,7 +126,6 @@ export default function ExperimentsSelection(props) {
         },
     ];
 
-// rowSelection object indicates the need for row selection
     const rowSelection = {
         onChange: (selectedRowKeys, selectedRows) => {
             console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
@@ -36,9 +133,17 @@ export default function ExperimentsSelection(props) {
         },
     };
 
+    const components = {
+        body: {
+            row: EditableRow,
+            cell: EditableCell,
+        },
+    };
+
     return (
         <>
             <Table
+                components={components}
                 rowSelection={{
                     type: 'checkbox',
                     selectedRowKeys: selectedRowsState,
