b82f657cb60fd6d00f190669cc1ed9a84a2fcdbf
Roman Mylonas <roman.mylonas@sib.swiss>
2022-03-25 16:32:50 +0100

    Show random echarts plot.

--- src/analysis_steps/boxplot/BoxPlot.js
+++ src/analysis_steps/boxplot/BoxPlot.js
@@ -1,15 +1,38 @@
 import React from "react";
 import {Card} from "antd";
 import AnalysisMenu from "../AnalysisMenu";
+import ReactECharts from 'echarts-for-react';
 
 export default function BoxPlot(props) {
     const results = JSON.parse(props.data.results)
+
+    const options = {
+        grid: { top: 8, right: 8, bottom: 24, left: 36 },
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+            type: 'value',
+        },
+        series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true,
+            },
+        ],
+        tooltip: {
+            trigger: 'axis',
+        },
+    };
 
     return (
         <Card title={"Boxplot"} headStyle={{textAlign: 'left'}} bodyStyle={{textAlign: 'left'}} extra={
             <AnalysisMenu stepId={props.data.id} resultId={props.resultId} status={props.data.status}/>
         }>
             <p>Boxplot</p>
+            <ReactECharts option={options} />
         </Card>
     );
 }
