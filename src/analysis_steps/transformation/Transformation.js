4645ca98678e9b6c628ec07300388a5e8769a496
Roman Mylonas <roman.mylonas@sib.swiss>
2022-04-28 15:17:55 +0200

    Add transformation params form.

--- src/analysis_steps/transformation/Transformation.js
+++ src/analysis_steps/transformation/Transformation.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import {Card} from "antd";
 import AnalysisMenu from "../AnalysisMenu";
 import TransformationParams from "./TransformationParams";
 
 export default function Transformation(props) {
+    const params = JSON.parse(props.data.parameters)
 
+    const [localParams, setLocalParams] = useState(params)
 
     const onClickOk = () => {
-        console.log("click OK")
+        console.log("click OK", localParams)
     }
 
     return (
@@ -16,9 +18,17 @@ export default function Transformation(props) {
             <AnalysisMenu stepId={props.data.id} resultId={props.resultId} status={props.data.status}
                           onClickOk={onClickOk}
                           paramComponent={<TransformationParams analysisIdx={props.analysisIdx}
-                                                         data={props.data}></TransformationParams>}/>
+                                                                data={props.data}
+                                                                localParams={localParams}
+                                                                setLocalParams={setLocalParams}></TransformationParams>}/>
         }>
-            <p>Coucou</p>
+            {params &&
+                <div>
+                    <p>Transformation: <strong>{params.transformationType}</strong></p>
+                    <p>Normalization: <strong>{params.normalizationType}</strong></p>
+                    <p>Imputation: <strong>{params.imputationType}</strong></p>
+                </div>
+            }
         </Card>
     );
 }
--- src/analysis_steps/transformation/TransformationParams.js
+++ src/analysis_steps/transformation/TransformationParams.js
@@ -1,37 +1,67 @@
 import React from "react";
-import {Checkbox, Select} from 'antd';
+import {Select} from 'antd';
 
 const {Option} = Select;
 
 export default function TransformationParams(props) {
 
-    const numCols = props.data.commonResult.numericalColumns
-    const intCol = numCols.findIndex(c => {
-         const selCol = props.selCol ? props.selCol : props.data.commonResult.intCol
-        return  selCol === c
-    })
+    const results = JSON.parse(props.data.results)
+    const numCols = (results && results.oldNumCols) ? results.oldNumCols : props.data.commonResult.numericalColumns
 
     function handleChange(value) {
-        props.setSelCol(numCols[value])
+        props.setLocalParams({...props.localParams, intCol: numCols[value]})
     }
 
-    function checkboxChange(e){
-        console.log(`checked = ${e.target.checked}`);
-        props.setLogScale(e.target.checked)
+    function transChange(value) {
+        props.setLocalParams({...props.localParams, transformationType: value})
     }
+
+    function normChange(value) {
+        props.setLocalParams({...props.localParams, normalizationType: value})
+    }
+
+    function impChange(value) {
+        props.setLocalParams({...props.localParams, imputationType: value})
+    }
+
+    const intColName = (results && results.oldIntCol) ? results.oldIntCol : props.data.commonResult.intCol
+    const intCol = numCols.findIndex(c => {
+        return intColName === c
+    })
+
 
     return (
         <>
-            <h3>Select data column for Boxplot</h3>
+            <h3>Select value to transform</h3>
             <Select defaultValue={intCol} style={{width: 250}} onChange={handleChange}>
                 {numCols.map((n, i) => {
-                    return <Option key={i} value={i}>{n}</Option>
-                })}
+                        return <Option key={i} value={i}>{n}</Option>
+                    })}
             </Select>
             <br></br>
-            <Checkbox
-                onChange={checkboxChange} defaultChecked={props.logScale}>Use logarithmic scale (log2)
-            </Checkbox>
+            <br></br>
+            <h3>Transformation</h3>
+            <Select defaultValue={props.localParams.transformationType} style={{width: 250}} onChange={transChange}>
+                    <Option value={'none'}>None</Option>
+                    <Option value={'log2'}>Log2</Option>
+            </Select>
+            <br></br>
+            <br></br>
+            <h3>Normalization</h3>
+            <Select defaultValue={props.localParams.normalizationType} style={{width: 250}} onChange={normChange}>
+                <Option value={'none'}>None</Option>
+                <Option value={'median'}>Median</Option>
+                {/*<Option value={'mean'}>Mean</Option>*/}
+            </Select>
+            <br></br>
+            <br></br>
+            <h3>Imputation</h3>
+            <Select defaultValue={props.localParams.imputationType} style={{width: 250}} onChange={impChange}>
+                <Option value={'none'}>None</Option>
+                <Option value={'nan'}>NaN (not a number)</Option>
+                {/*<Option value={'normal'}>Normal distribution</Option>
+                <Option value={'value'}>Fix value</Option>*/}
+            </Select>
 
         </>
     );
