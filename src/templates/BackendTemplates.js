b7ccb30d423578c2438d4cfe2411c0331877abae
Roman Mylonas <romanoman@gmail.com>
2022-06-22 08:48:15 +0200

    Update template fields.

--- src/templates/BackendTemplates.js
+++ src/templates/BackendTemplates.js
@@ -1,27 +1,45 @@
 import axios from 'axios';
 import globalConfig from "../globalConfig";
 import {createAsyncThunk} from "@reduxjs/toolkit";
+import {fetchAnalysisByResultId} from "../analysis/BackendAnalysis";
 
-function getAllTemplates(){
+function getAllTemplates() {
     return axios.get(globalConfig.urlBackend + "template")
 }
 
-export function deleteAnalysis(analysisId){
+export function deleteAnalysis(analysisId) {
     return axios.delete(globalConfig.urlBackend + "analysis/" + analysisId)
 }
 
-export const fetchAllTemplates = createAsyncThunk(
-    'templates/fetch-all',
-    async (resultId, thunkApi) => {
-        try {
-            const response = await getAllTemplates()
-            return response.data
-        }catch(err){
-            let error = err // cast the error for access
-            if (!error.response) {
-                throw err
-            }
-            return thunkApi.rejectWithValue(error.response.data)
+export const fetchAllTemplates = createAsyncThunk('templates/fetch-all', async (resultId, thunkApi) => {
+    try {
+        const response = await getAllTemplates()
+        return response.data
+    } catch (err) {
+        let error = err // cast the error for access
+        if (!error.response) {
+            throw err
         }
+        return thunkApi.rejectWithValue(error.response.data)
     }
-)
+})
+
+function updateTemplateCall(templateObj) {
+    return axios.post(globalConfig.urlBackend + "template/update/" + templateObj.id + "?name=" + templateObj.name + "&value=" + encodeURIComponent(templateObj.value))
+}
+
+export const updateTemplate = createAsyncThunk('templates/update', async (templateObj, thunkApi) => {
+    try {
+        const response = await updateTemplateCall(templateObj)
+        return response.data
+    } catch (err) {
+        let error = err // cast the error for access
+        if (!error.response) {
+            throw err
+        }
+        return thunkApi.rejectWithValue(error.response.data)
+    } finally {
+        thunkApi.dispatch(fetchAllTemplates())
+    }
+})
+
--- src/templates/TemplatesTable.js
+++ src/templates/TemplatesTable.js
@@ -1,25 +1,92 @@
-import React from "react";
-import {Layout, Space, Table} from "antd";
+import React, {useState} from "react";
+import {Button, Layout, message, Popconfirm, Space, Table, Input} from "antd";
+import {DeleteOutlined} from "@ant-design/icons";
+import {updateTemplate} from "./BackendTemplates";
+import {useDispatch} from "react-redux";
 
 const {Content} = Layout;
 
 export default function TemplatesTable(props) {
+    const [editField, setEditField] = useState(undefined);
+    const [editFieldValue, setEditFieldValue] = useState(undefined);
+    const dispatch = useDispatch();
+
+    const confirmDelete = (templateId) => {
+        console.log("Delete " + templateId)
+        message.success('Delete template [' + templateId + '].');
+    };
+
+    const editEntry = (record, field) => {
+        setEditField({id: record.id, field: field})
+        setEditFieldValue(record[field])
+    }
+
+    const saveEntry = () => {
+        if(editFieldValue && editField){
+            dispatch(updateTemplate({id: editField.id, name: editField.field, value: editFieldValue}))
+            setEditField(undefined)
+            setEditFieldValue(undefined)
+        }
+    }
+
+    const pressedKey = (ev) => {
+        if (ev.key === "Enter") {
+            saveEntry()
+        }
+    }
+
+    const renderEditField = (field) => {
+        return (_, record) => {
+            {
+                if (editField && editField.field === field && record.id === editField.id) {
+                    return (
+                        <Space onKeyPress={(ev) => pressedKey(ev)} onBlur={() => saveEntry()}>
+                            <Input defaultValue={_} onChange={(e) => setEditFieldValue(e.target.value)}/>
+                        </Space>
+                    )
+                } else {
+                    return (
+                        <Space>
+                            {_}
+                        </Space>
+                    )
+
+                }
+            }
+        }
+    }
+
+    const onCellEditField = (field) => {
+        return (record) => {
+            return {
+                onClick: () => {
+                    editEntry(record, field)
+                },
+            };
+        }
+    }
+
     const columns = [
         {
             title: 'Id',
             dataIndex: 'id',
             key: 'id',
-            render: (text) => <a>{text}</a>,
         },
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
+            width: '200px',
+            render: renderEditField('name'),
+            onCell: onCellEditField('name'),
         },
         {
             title: 'Description',
             dataIndex: 'description',
             key: 'description',
+            width: '200px',
+            render: renderEditField('description'),
+            onCell: onCellEditField('description'),
         },
         {
             title: 'Last modification',
@@ -37,6 +104,22 @@ export default function TemplatesTable(props) {
             title: 'Nr of steps',
             dataIndex: 'nrSteps',
             key: 'nrSteps',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <Popconfirm
+                        title="Are you sure to delete this step?"
+                        onConfirm={() => confirmDelete(record.id)}
+                        okText="Yes"
+                        cancelText="Cancel"
+                    >
+                        <Button type={"text"} icon={<DeleteOutlined/>}></Button>
+                    </Popconfirm>
+                </Space>
+            ),
         },
     ]
 
