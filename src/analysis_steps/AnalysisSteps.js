7ab4f535cb998853bf5862012a4d73d03089a834
Roman Mylonas <romanoman@gmail.com>
2022-09-16 10:37:28 +0200

    Relook code.

--- src/analysis_steps/AnalysisSteps.js
+++ src/analysis_steps/AnalysisSteps.js
@@ -2,8 +2,8 @@ import React, {useState} from "react";
 import InitialResult from "./initial_result/InitialResult";
 import QualityControl from "./quality_control/QualityControl";
 import Filter from "./filter/Filter"
-import {EllipsisOutlined} from "@ant-design/icons";
-import {Button, Dropdown, Badge} from "antd";
+import {EllipsisOutlined, SettingOutlined} from "@ant-design/icons";
+import {Button, Dropdown, Badge, Alert} from "antd";
 import BoxPlot from "./boxplot/BoxPlot";
 import './AnalysisStep.css'
 import Transformation from "./transformation/Transformation";
@@ -17,16 +17,28 @@ export default function AnalysisSteps(props) {
 
     const badgeStatus = () => {
         switch (props.data.status) {
-            case 'error': return 'error'
-            case 'running': return 'processing'
-            case 'idle': return 'warning'
-            case 'done': return 'success'
-            default: return 'default'
+            case 'error':
+                return 'error'
+            case 'running':
+                return 'processing'
+            case 'idle':
+                return 'warning'
+            case 'done':
+                return 'success'
+            default:
+                return 'default'
         }
     }
 
-    return (<div className={"analysis-col"}>
-            <h3><span style={{float: "left", marginLeft: "10px"}}><Badge status={badgeStatus()} /></span>Analysis #{props.data.idx + 1} <span style={{float: "right", marginRight: "10px"}}>
+    return (
+        <>
+            <div className={"analysis-col"}>
+                <h3>
+                <span style={{float: "left", marginLeft: "10px"}}>
+                    <Badge status={badgeStatus()}/>
+                </span>
+                    Analysis #{props.data.idx + 1}
+                    <span style={{float: "right", marginRight: "10px"}}>
                 <Dropdown visible={menuIsVisible} onClick={() => setMenuIsVisible(true)}
                           overlay={<AnalysisMenu analysisId={props.data.id} setMenuIsVisible={setMenuIsVisible}
                                                  resultsId={props.data.result.id}></AnalysisMenu>}
@@ -35,41 +47,42 @@ export default function AnalysisSteps(props) {
                     <Button type={"text"} icon={<EllipsisOutlined style={{fontSize: '24px'}}/>}></Button>
                 </Dropdown>
 
-            </span></h3>
-            <div className={"analysis-col-content"}>
-                {props.data.analysisSteps && props.data.analysisSteps.map(step => {
-                    switch (step.type) {
-                        case 'initial-result':
-                            return <InitialResult analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                                  data={step} key={step.id}/>
-                        case 'quality-control':
-                            return <QualityControl analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                                   data={step} key={step.id}/>
-                        case 'boxplot':
-                            return <BoxPlot analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                            data={step} key={step.id}/>
-                        case 'transformation':
-                            return <Transformation analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                                   data={step} key={step.id}/>
-                        case 'filter':
-                            return <Filter analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                                   data={step} key={step.id}/>
-                        case 'group-filter':
-                            return <GroupFilter analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                           data={step} key={step.id}/>
-                        case 't-test':
-                            return <TTest analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+            </span>
+                </h3>
+                <div className={"analysis-col-content"}>
+                    {props.data.analysisSteps && props.data.analysisSteps.map(step => {
+                        switch (step.type) {
+                            case 'initial-result':
+                                return <InitialResult analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                                      data={step} key={step.id}/>
+                            case 'quality-control':
+                                return <QualityControl analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                                       data={step} key={step.id}/>
+                            case 'boxplot':
+                                return <BoxPlot analysisIdx={props.analysisIdx} resultId={props.data.result.id}
                                                 data={step} key={step.id}/>
-                        case 'volcano-plot':
-                            return <VolcanoPlot analysisIdx={props.analysisIdx} resultId={props.data.result.id}
-                                          data={step} key={step.id}/>
+                            case 'transformation':
+                                return <Transformation analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                                       data={step} key={step.id}/>
+                            case 'filter':
+                                return <Filter analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                               data={step} key={step.id}/>
+                            case 'group-filter':
+                                return <GroupFilter analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                                    data={step} key={step.id}/>
+                            case 't-test':
+                                return <TTest analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                              data={step} key={step.id}/>
+                            case 'volcano-plot':
+                                return <VolcanoPlot analysisIdx={props.analysisIdx} resultId={props.data.result.id}
+                                                    data={step} key={step.id}/>
 
-                        default:
-                            return null
-                    }
-                })}
+                            default:
+                                return null
+                        }
+                    })}
+                </div>
             </div>
-        </div>
-
+        </>
     );
 }
