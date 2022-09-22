80f4131612a894231e45c4f3db6e66cb8935d92c
Roman Mylonas <romanoman@gmail.com>
2022-09-22 17:18:51 +0200

    Remove analysisStepParamSlice.

--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import analysisReducer from './analysis/analysisSlice'
 import templateReducer from './templates/templatesSlice'
-import analysisStepParamsReducer from "./analysis_steps/analysisStepParamsSlice";
 import proteinTableReducer from "./protein_table/proteinTableSlice";
 
 export default configureStore({
     reducer: {
         templates: templateReducer,
         analysis: analysisReducer,
-        analysisStepParams: analysisStepParamsReducer,
         proteinTable: proteinTableReducer
     }
 })
