26ede5c4c8d0dd8436526a88c909a54532a575b3
Roman Mylonas <romanoman@gmail.com>
2023-01-17 14:17:05 +0100

    Deactivate Spectronaut.

--- src/results/BrowseResultsModal.js
+++ src/results/BrowseResultsModal.js
@@ -91,7 +91,7 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel, availableDirs }) =>
                 <Form.Item name="type" className="collection-create-form_last-form-item" initialValue={initialType}>
                     <Radio.Group onChange={onChangeType}>
                         <Radio value="MaxQuant">MaxQuant</Radio>
-                        <Radio value="Spectronaut">Spectronaut</Radio>
+                        <Radio disabled={true} value="Spectronaut">Spectronaut</Radio>
                     </Radio.Group>
                 </Form.Item>
             </Form>
