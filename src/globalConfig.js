be1b9bc8097c75e7b328840d1624f5422ad1b750
Roman Mylonas <romanoman@gmail.com>
2023-04-25 10:50:43 +0200

    Change selection color.

--- src/globalConfig.js
+++ src/globalConfig.js
@@ -6,7 +6,7 @@ let globalConfig = {};
 
 globalConfig.version = '0.5.5'
 
-//globalConfig.urlBackend = "http://localhost:8080/"
-globalConfig.urlBackend = "http://taram-dev.dcsr.unil.ch/backend/"
+globalConfig.urlBackend = "http://localhost:8080/"
+//globalConfig.urlBackend = "http://taram-dev.dcsr.unil.ch/backend/"
 
 module.exports = globalConfig;
