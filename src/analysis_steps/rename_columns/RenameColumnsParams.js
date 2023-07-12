10e78435ea564c0c5c420619f54e9a2eafe85421
Roman Mylonas <romanoman@gmail.com>
2023-07-12 16:52:50 +0200

    Add condition names.

--- src/analysis_steps/rename_columns/RenameColumnsParams.js
+++ src/analysis_steps/rename_columns/RenameColumnsParams.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Tree, Tag, Input, Button} from 'antd';
+import {Tree, Tag, Input, Button, Checkbox} from 'antd';
 import {CheckOutlined, CloseOutlined} from "@ant-design/icons";
 
 export default function RenameColumnsParams(props) {
@@ -8,6 +8,8 @@ export default function RenameColumnsParams(props) {
     const [editName, setEditName] = useState()
     const [treeData, setTreeData] = useState()
 
+    const groupsDefined = (Object.values(props.experimentDetails).find(a => a.group)) ? true : false
+
     useEffect(() => {
         setTreeData(computeTreeData())
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -15,27 +17,27 @@ export default function RenameColumnsParams(props) {
 
     const computeTreeData = () => {
         return props.commonResult.headers.reduce((acc, val) => {
-            if(val.experiment){
-                const renameItem = (props.params && props.params.rename) ? props.params.rename.find( r => r.idx === val.experiment.field) : undefined
+            if (val.experiment) {
+                const renameItem = (props.params && props.params.rename) ? props.params.rename.find(r => r.idx === val.experiment.field) : undefined
                 const fieldName = renameItem ? renameItem.name : val.experiment.field
 
-                const newVal =   {
+                const newVal = {
                     title: renameItem ? val.experiment.name + "." + renameItem.name : val.name,
                     key: val.idx,
                     type: val.type,
                     inExperiment: true
                 }
 
-                const treeIdx = acc.findIndex( a => a.key === fieldName)
-                if(treeIdx < 0){
+                const treeIdx = acc.findIndex(a => a.key === fieldName)
+                if (treeIdx < 0) {
                     acc.push({title: fieldName, key: fieldName, type: 'EXPERIMENT', children: [newVal]})
-                }else{
+                } else {
                     acc[treeIdx] = {...acc[treeIdx], children: acc[treeIdx].children.concat(newVal)}
                 }
-            }else{
-                const renameItem = (props.params && props.params.rename) ? props.params.rename.find( r => r.idx === val.idx) : undefined
+            } else {
+                const renameItem = (props.params && props.params.rename) ? props.params.rename.find(r => r.idx === val.idx) : undefined
 
-                const newVal =   {
+                const newVal = {
                     title: (renameItem ? renameItem.name : val.name),
                     key: val.idx,
                     type: val.type
@@ -49,20 +51,20 @@ export default function RenameColumnsParams(props) {
     }
 
     const getTag = (type) => {
-        if(type === "NUMBER"){
+        if (type === "NUMBER") {
             return <Tag color={"green"}>Numerical</Tag>
-        }else if(type === "CHARACTER"){
+        } else if (type === "CHARACTER") {
             return <Tag color={"gold"}>Character</Tag>
-        }else if(type === "EXPERIMENT"){
+        } else if (type === "EXPERIMENT") {
             return null
-        }else{
+        } else {
             return null
         }
 
     }
 
     const onClick = (e, a) => {
-        if(!a.node.inExperiment){
+        if (!a.node.inExperiment) {
             setEditName(a.node.title)
             setSelItem(a.node)
         }
@@ -91,23 +93,45 @@ export default function RenameColumnsParams(props) {
 
     const renderTitle = item => {
         // if we're editing we give back the input
-        if(selItem && selItem.key === item.key){
+        if (selItem && selItem.key === item.key) {
             return <div key={item.key} style={{whiteSpace: "nowrap"}}>
                 <Input defaultValue={item.title}
                        onPressEnter={save}
                        onChange={(e) => setName(e.target.value)}
                        onBlur={save}/>
-                    <Button shape="circle" icon={<CheckOutlined style={{fontSize: '10px'}}/>} onClick={(e) => save(e)} size={"small"}></Button>
-                    <Button shape="circle" icon={<CloseOutlined style={{fontSize: '10px'}}/>} onClick={(e) => cancel(e)} size={"small"} danger></Button>
+                <Button shape="circle" icon={<CheckOutlined style={{fontSize: '10px'}}/>} onClick={(e) => save(e)}
+                        size={"small"}></Button>
+                <Button shape="circle" icon={<CloseOutlined style={{fontSize: '10px'}}/>} onClick={(e) => cancel(e)}
+                        size={"small"} danger></Button>
             </div>
         }
 
         //otherwise we give back the node element
-      return <div key={item.key}><span>{item.title}&nbsp;&nbsp;&nbsp;{getTag(item.type)}</span></div>
+        return <div key={item.key}><span>{item.title}&nbsp;&nbsp;&nbsp;{getTag(item.type)}</span></div>
+    }
+
+    const reset = () => {
+        props.setParams(undefined)
+        setSelItem(undefined)
+        setEditName(undefined)
+    }
+
+    const handleChange = (status) => {
+        props.setParams({...props.params, addConditionNames: status})
     }
 
     return (
         <>
+            <div><Button type="primary" onClick={() => reset()}>Reset</Button></div>
+            <br></br>
+            <span>
+                <Checkbox disabled={!groupsDefined} checked={props.params && props.params.addConditionNames}
+                          onChange={(e) => handleChange(e.target.checked)}>
+                </Checkbox>
+                <span style={{paddingLeft: "20px"}}>Add condition names to columns.</span>
+            </span>
+            <br></br>
+            <br></br>
             <h3>Only keep selected columns</h3>
             <Tree
                 treeData={treeData}
