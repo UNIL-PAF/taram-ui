daff200313cc140eb99c6d3dac0dc3f1b12f0621
Roman Mylonas <romanoman@gmail.com>
2023-08-17 16:40:00 +0200

    Show labels on PCA plot.

--- src/analysis_steps/pca/PcaPlot.js
+++ src/analysis_steps/pca/PcaPlot.js
@@ -8,6 +8,7 @@ import StepComment from "../StepComment";
 import {FullscreenOutlined} from "@ant-design/icons";
 import EchartsZoom from "../EchartsZoom";
 import {typeToName} from "../TypeNameMapping"
+import {switchSel} from "../BackendAnalysisSteps";
 
 export default function PcaPlot(props) {
     const type = 'pca'
@@ -15,7 +16,14 @@ export default function PcaPlot(props) {
     const [options, setOptions] = useState()
     const [isWaiting, setIsWaiting] = useState(true)
     const [showZoom, setShowZoom] = useState(null)
+    const [onEvents, setOnEvents] = useState()
+    const [selExps, setSelExps] = useState([])
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        setOnEvents({'click': showToolTipOnClick})
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selExps])
 
     useEffect(() => {
         if (props.data) {
@@ -24,7 +32,9 @@ export default function PcaPlot(props) {
 
             if (isWaiting && props.data.status === 'done') {
                 const results = JSON.parse(props.data.results)
-                const echartOptions = getOptions(results)
+                const backendSelExps = JSON.parse(props.data.parameters).selExps
+                if (backendSelExps) setSelExps(backendSelExps)
+                const echartOptions = getOptions(results, params, backendSelExps)
                 replacePlotIfChanged(props.data.id, results, echartOptions, dispatch)
                 setOptions({count: options ? options.count + 1 : 0, data: echartOptions})
                 setIsWaiting(false)
@@ -39,7 +49,7 @@ export default function PcaPlot(props) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props, isWaiting])
 
-    const getOptions = (results) => {
+    const getOptions = (results, params, mySelExps) => {
         const xAxisPc = 0
         const yAxisPc = 1
 
@@ -47,26 +57,66 @@ export default function PcaPlot(props) {
             return {transform: {type: 'filter', config: {dimension: 'group', value: g}}}
         })
 
-        const series = results.groups.map((g, i) => {
+        const labels = [
+            {
+                label: {
+                    show: true,
+                    formatter: function (v) {
+                        return v.value[0]
+                    },
+                    position: 'right',
+                    minMargin: 2,
+                    fontWeight: 'bold',
+                    fontSize: 14,
+                    color: 'black'
+
+                },
+                symbolSize: 10,
+                itemStyle: {
+                    color: "rgba(0, 128, 0, 0)",
+                    borderWidth: 1,
+                    borderColor: 'grey'
+                },
+                datasetIndex: 1,
+                type: 'scatter',
+                encode: {
+                    x: 'x',
+                    y: 'y',
+                },
+            },
+        ]
+
+        const series = labels.concat(results.groups.map((g, i) => {
             return  {
                 name: g,
-                datasetIndex: (i + 1),
+                datasetIndex: (i + 2),
                 type: 'scatter',
                 encode: {
                     x: 'x',
                     y: 'y'
                 }
             }
+        }))
+
+        const defSelExps = (mySelExps ? mySelExps : params.selExps)
+
+        const dataWithLabel = results.pcList.map(p => {
+            const showLab = defSelExps && defSelExps.includes(p.expName)
+            return [p.expName, p.groupName, p.pcVals[xAxisPc], p.pcVals[yAxisPc], showLab]
         })
 
         const options = {
             dataset: [
                 {
-                    dimensions: ["expName", "group", "x", "y"],
-                    source: results.pcList.map(p => {
-                        return [p.expName, p.groupName, p.pcVals[xAxisPc], p.pcVals[yAxisPc]]
-                    }),
-                }
+                    dimensions: ["expName", "group", "x", "y", "showLab"],
+                    source: dataWithLabel
+                },
+                {
+                    transform: {
+                        type: 'filter',
+                        config: {dimension: 'showLab', value: true}
+                    }
+                },
             ].concat(transforms),
             xAxis: {
                 name: "PC" + (xAxisPc + 1) + " (" + results.explVars[xAxisPc].toFixed(1) + "%)",
@@ -98,6 +148,21 @@ export default function PcaPlot(props) {
         return options
     }
 
+    function showToolTipOnClick(e) {
+        const exp = e.data[0]
+        const expIndex = (selExps ? selExps.indexOf(exp) : -1)
+        const newSelExps = expIndex > -1 ? selExps.filter(e => e !== exp) : selExps.concat(exp)
+        setSelExps(newSelExps)
+
+        const results = JSON.parse(props.data.results)
+        const echartOptions = getOptions(results, localParams, newSelExps)
+        const callback = () => {
+            replacePlotIfChanged(props.data.id, results, echartOptions, dispatch)
+        }
+        dispatch(switchSel({resultId: props.resultId, selId: exp, stepId: props.data.id, callback: callback}))
+        setOptions({count: options ? options.count + 1 : 0, data: echartOptions})
+    }
+
     return (
         <Card className={"analysis-step-card" + (props.isSelected ? " analysis-step-sel" : "")}
               onClick={props.onSelect}
@@ -127,10 +192,11 @@ export default function PcaPlot(props) {
             </div>}
             {props.data.copyDifference && <span className={'copy-difference'}>{props.data.copyDifference}</span>}
             {options && options.data && options.data.series.length > 0 &&
-                <ReactECharts key={options.count} option={options.data}/>}
+                <ReactECharts key={options.count} option={options.data} onEvents={onEvents}/>}
             <StepComment stepId={props.data.id} resultId={props.resultId} comment={props.data.comments}></StepComment>
             {options && <EchartsZoom showZoom={showZoom} setShowZoom={setShowZoom} echartsOptions={options.data}
-                                     paramType={type} stepId={props.data.id} minHeight={"800px"}></EchartsZoom>}
+                                     paramType={type} stepId={props.data.id} minHeight={"800px"}
+                                     onEvents={onEvents}></EchartsZoom>}
         </Card>
     );
 }
