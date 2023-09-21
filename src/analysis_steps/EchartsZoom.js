f21bf7927445b521a98d1b1b34975be3c662dfd0
Roman Mylonas <romanoman@gmail.com>
2023-09-21 09:41:05 +0200

    Squeezed zoom plot bug: Add a small timeout.

--- src/analysis_steps/EchartsZoom.js
+++ src/analysis_steps/EchartsZoom.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Modal} from "antd";
 import '../analysis/analysis.css'
 import ReactECharts from "echarts-for-react";
 
 export default function EchartsZoom(props) {
+
+    const [myTimeout, setMyTimeout] = useState(true)
+
+    useEffect(() => {
+        if(props.showZoom && myTimeout && props.echartsOptions){
+            setTimeout(() => {
+                setMyTimeout(false)
+            }, 100)
+        }
+    }, [props, myTimeout])
 
     const showZoomModal = () => {
         return <>
@@ -35,7 +45,7 @@ export default function EchartsZoom(props) {
         <Modal open={props.showZoom} title="Plot" onCancel={() => props.setShowZoom(false)}
                width={"95%"} height={"100%"} footer={null}
         >
-            {showZoomModal()}
+            {!myTimeout && showZoomModal()}
         </Modal>
     );
 }
