d67edeae6e3e54774d58bf5c5c701ede994bd822
Roman Mylonas <romanoman@gmail.com>
2023-11-10 08:25:33 +0100

    Add feature: Choisi de faire imputation seulement sur les colonnes que on veut.

--- src/analysis_steps/imputation/ImputationParams.js
+++ src/analysis_steps/imputation/ImputationParams.js
@@ -1,42 +1,79 @@
 import React, {useEffect, useState} from "react";
-import {Checkbox, InputNumber, Select, Space} from 'antd';
-import {getNumCols} from "../CommonStepUtils";
+import {InputNumber, Row, Select, Space, Switch, Tree, Col} from 'antd';
 
 const {Option} = Select;
 
 export default function ImputationParams(props) {
-
-    const numCols = getNumCols(props.commonResult.headers)
-    const intColName = props.commonResult.intCol
     const [useDefaultCol, setUseDefaultCol] = useState()
+    const [colData, setColData] = useState()
+    const [checkedKeys, setCheckedKeys] = useState();
 
     useEffect(() => {
-        if(!props.params){
-            props.setParams({
-                intCol: intColName,
-                imputationType: 'normal',
-                normImputationParams: {
-                    width: 0.3,
-                    downshift: 1.8,
-                    seed: 1
+            if (!props.params) {
+                props.setParams({
+                    intCol: null,
+                    imputationType: 'normal',
+                    normImputationParams: {
+                        width: 0.3,
+                        downshift: 1.8,
+                        seed: 1
+                    }
+                })
+                setUseDefaultCol(true)
+                setCheckedKeys([props.intCol])
+            } else {
+                if (useDefaultCol === undefined) {
+                    if (props.params.intCol === null) {
+                        if(!props.params.selColIdxs){
+                            setUseDefaultCol(true)
+                            setCheckedKeys([props.intCol])
+                        }else{
+                            setUseDefaultCol(false)
+                            setCheckedKeys(props.params.selColIdxs)
+                        }
+                    } else {
+                        setUseDefaultCol(false)
+                        setCheckedKeys([props.params.intCol])
+                    }
                 }
-            })
-            setUseDefaultCol(true)
-        }else{
-            if(useDefaultCol === undefined){
-                setUseDefaultCol(props.params.inCol ? false: true)
+
+                if (!colData) {
+                    const myColData = computeColData()
+                    setColData(myColData)
+                }
             }
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props, useDefaultCol])
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [props.params, useDefaultCol, colData]
+    )
 
-    function handleChange(value) {
-        props.setParams({...props.params, intCol: numCols[value]})
-    }
+    const computeColData = () => {
+        return props.commonResult.headers.reduce((acc, val) => {
+            const newVal = {
+                title: val.name,
+                key: val.idx,
+                type: val.type
+            }
 
-    function changeUseDefaultCol(e){
-        setUseDefaultCol(e.target.checked)
-        if(e.target.checked) props.setParams({...props.params, intCol: null})
+            if (val.type !== "NUMBER") return acc
+
+            if (val.experiment) {
+                const treeIdx = acc.findIndex(a => a.key === val.experiment.field)
+                if (treeIdx < 0) {
+                    acc.push({
+                        title: val.experiment.field,
+                        key: val.experiment.field,
+                        type: val.type,
+                        children: [newVal]
+                    })
+                } else {
+                    acc[treeIdx] = {...acc[treeIdx], children: acc[treeIdx].children.concat(newVal)}
+                }
+            } else {
+                acc.push(newVal)
+            }
+
+            return acc
+        }, [])
     }
 
     function impChange(value) {
@@ -69,29 +106,55 @@ export default function ImputationParams(props) {
             </span>
     }
 
-    function showOptions(){
-        return <>
-            <Space direction="vertical" size="middle">
-                <Checkbox
-                    onChange={changeUseDefaultCol} checked={useDefaultCol}>Use default intensity values [{props.intCol}]
-                </Checkbox>
-                <Select disabled={useDefaultCol} value={props.params.intCol || props.intCol} style={{width: 250}} onChange={handleChange}>
-                    {numCols.map((n, i) => {
-                        return <Option key={i} value={i}>{n}</Option>
-                    })}
-                </Select>
-                <h3>Imputation</h3>
-                <span>
+    const onCheck = (e) => {
+        setCheckedKeys(e)
+        if (e.length > 0) {
+            if (typeof e[0] === "string") {
+                props.setParams({...props.params, intCol: e[0], selColIdxs: null})
+            } else {
+                props.setParams({...props.params, intCol: null, selColIdxs: e})
+            }
+        }
+    }
+
+    const switchDefault = (e) => {
+        setUseDefaultCol(e)
+        if (e) {
+            props.setParams({...props.params, intCol: null, selColIdxs: null})
+            setCheckedKeys([props.intCol])
+        } else setCheckedKeys(undefined)
+    }
+
+    function showOptions() {
+        return <Row>
+            <Col span={12}>
+                <h3>Select columns</h3>
+                <span><Switch onChange={switchDefault}
+                              checked={useDefaultCol}></Switch>&nbsp;Use default intensity values
+                    [{props.intCol}]</span><br></br><br></br>
+                <Tree
+                    checkable
+                    disabled={useDefaultCol}
+                    treeData={colData}
+                    onCheck={onCheck}
+                    checkedKeys={checkedKeys}
+                />
+            </Col>
+            <Col span={12}>
+                <Space direction="vertical" size="middle">
+                    <h3>Imputation</h3>
+                    <span>
                 <Select value={props.params.imputationType} style={{width: 250}} onChange={impChange}>
                     <Option value={'normal'}>Normal distribution</Option>
                     <Option value={'nan'}>Replace by NaN</Option>
                     <Option value={'value'}>Fix value</Option>
                 </Select>
-                    {props.params.imputationType === "value" && valueParams()}
-                    {props.params.imputationType === "normal" && normalParams()}
+                        {props.params.imputationType === "value" && valueParams()}
+                        {props.params.imputationType === "normal" && normalParams()}
                 </span>
-            </Space>
-        </>
+                </Space>
+            </Col>
+        </Row>
     }
 
     return (
