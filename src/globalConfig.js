eadb99f6ee7b884da0b64a61a3658c9cf3874462
Roman Mylonas <romanoman@gmail.com>
2024-07-12 16:09:09 +0200

    Version 2.3.3

--- src/globalConfig.js
+++ src/globalConfig.js
@@ -4,9 +4,9 @@
 
 let globalConfig = {};
 
-globalConfig.version = 'v2.3.1'
+globalConfig.version = 'v2.3.3'
 
-globalConfig.urlBackend = "http://localhost:8080/"
-//globalConfig.urlBackend = "http://taram-dev.dcsr.unil.ch/backend/"
+//globalConfig.urlBackend = "http://localhost:8080/"
+globalConfig.urlBackend = "http://taram-dev.dcsr.unil.ch/backend/"
 
 module.exports = globalConfig;
